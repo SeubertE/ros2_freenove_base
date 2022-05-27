1ac41011e7f626f23e15aefc2ce71286df8aecc7
Eugene Seubert <eugene.seubert@gmail.com>
2022-05-26 21:02:53 -0700

    inital commit - working port of original code to ros2

---
+++ freenove_base/driver4hardware.py
@@ -0,0 +1,279 @@
+#!/usr/bin/env python
+import time
+import smbus
+from time import sleep
+import RPi.GPIO as GPIO
+from rpi_ws281x import *
+from .PCA9685 import PCA9685
+
+class cmd:
+    CMD_MOTOR = "CMD_MOTOR"
+    CMD_LED = "CMD_LED"
+    CMD_LED_MOD = "CMD_LED_MOD"
+    CMD_SERVO = "CMD_SERVO"
+    CMD_BUZZER = "CMD_BUZZER"
+    CMD_SONIC = "CMD_SONIC"
+    CMD_LIGHT = "CMD_LIGHT"
+    CMD_POWER = "CMD_POWER" 
+    CMD_MODE ="CMD_MODE"
+    def __init__(self):
+        pass
+
+# Class for moveing control
+# execute motor module
+#  channel: Front left, Back left, Front right, Back right
+class Motor:
+    def __init__(self):
+        self.pwm = PCA9685(0x40, debug=True)
+        self.pwm.setPWMFreq(50)
+    def duty_range(self,duty1,duty2,duty3,duty4):
+        if duty1>4095:
+            duty1=4095
+        elif duty1<-4095:
+            duty1=-4095        
+        
+        if duty2>4095:
+            duty2=4095
+        elif duty2<-4095:
+            duty2=-4095
+            
+        if duty3>4095:
+            duty3=4095
+        elif duty3<-4095:
+            duty3=-4095
+            
+        if duty4>4095:
+            duty4=4095
+        elif duty4<-4095:
+            duty4=-4095
+        return duty1,duty2,duty3,duty4
+        
+    def left_Upper_Wheel(self,duty):
+        if duty>0:
+            self.pwm.setMotorPwm(0,0)
+            self.pwm.setMotorPwm(1,duty)
+        elif duty<0:
+            self.pwm.setMotorPwm(1,0)
+            self.pwm.setMotorPwm(0,abs(duty))
+        else:
+            self.pwm.setMotorPwm(0,4095)
+            self.pwm.setMotorPwm(1,4095)
+    def left_Lower_Wheel(self,duty):
+        if duty>0:
+            self.pwm.setMotorPwm(3,0)
+            self.pwm.setMotorPwm(2,duty)
+        elif duty<0:
+            self.pwm.setMotorPwm(2,0)
+            self.pwm.setMotorPwm(3,abs(duty))
+        else:
+            self.pwm.setMotorPwm(2,4095)
+            self.pwm.setMotorPwm(3,4095)
+    def right_Upper_Wheel(self,duty):
+        if duty>0:
+            self.pwm.setMotorPwm(6,0)
+            self.pwm.setMotorPwm(7,duty)
+        elif duty<0:
+            self.pwm.setMotorPwm(7,0)
+            self.pwm.setMotorPwm(6,abs(duty))
+        else:
+            self.pwm.setMotorPwm(6,4095)
+            self.pwm.setMotorPwm(7,4095)
+    def right_Lower_Wheel(self,duty):
+        if duty>0:
+            self.pwm.setMotorPwm(4,0)
+            self.pwm.setMotorPwm(5,duty)
+        elif duty<0:
+            self.pwm.setMotorPwm(5,0)
+            self.pwm.setMotorPwm(4,abs(duty))
+        else:
+            self.pwm.setMotorPwm(4,4095)
+            self.pwm.setMotorPwm(5,4095)            
+ 
+    def setMotorModel(self,duty1,duty2,duty3,duty4):
+        duty1,duty2,duty3,duty4=self.duty_range(duty1,duty2,duty3,duty4)
+        self.left_Upper_Wheel(-duty1)
+        self.left_Lower_Wheel(-duty2)
+        self.right_Upper_Wheel(-duty3)
+        self.right_Lower_Wheel(-duty4)
+
+# Class for the voltage values of the two photoresistors 
+# and the battery are output
+# sensor module
+#   channel: light, right, power
+class Adc:
+    def __init__(self):
+        # Get I2C bus
+        self.bus = smbus.SMBus(1)
+        
+        # I2C address of the device
+        self.ADDRESS            = 0x48
+        
+        # PCF8591 Command
+        self.PCF8591_CMD                        =0x40  #Command
+        
+        # ADS7830 Command 
+        self.ADS7830_CMD                        = 0x84 # Single-Ended Inputs
+        
+        for i in range(3):
+            aa=self.bus.read_byte_data(self.ADDRESS,0xf4)
+            if aa < 150:
+                self.Index="PCF8591"
+            else:
+                self.Index="ADS7830" 
+    def analogReadPCF8591(self,chn):#PCF8591 read ADC value,chn:0,1,2,3
+        value=[0,0,0,0,0,0,0,0,0]
+        for i in range(9):
+            value[i] = self.bus.read_byte_data(self.ADDRESS,self.PCF8591_CMD+chn)
+        value=sorted(value)
+        return value[4]   
+        
+    def analogWritePCF8591(self,value):#PCF8591 write DAC value
+        self.bus.write_byte_data(self.ADDRESS,cmd,value)
+        
+    def recvPCF8591(self,channel):#PCF8591 write DAC value
+        while(1):
+            value1 = self.analogReadPCF8591(channel)   #read the ADC value of channel 0,1,2,
+            value2 = self.analogReadPCF8591(channel)
+            if value1==value2:
+                break
+        voltage = value1 / 256.0 * 3.3  #calculate the voltage value
+        voltage = round(voltage,2)
+        return voltage
+    def recvADS7830(self,channel):
+        """Select the Command data from the given provided value above"""
+        COMMAND_SET = self.ADS7830_CMD | ((((channel<<2)|(channel>>1))&0x07)<<4)
+        self.bus.write_byte(self.ADDRESS,COMMAND_SET)
+        while(1):
+            value1 = self.bus.read_byte(self.ADDRESS)
+            value2 = self.bus.read_byte(self.ADDRESS)
+            if value1==value2:
+                break
+        voltage = value1 / 255.0 * 3.3  #calculate the voltage value
+        voltage = round(voltage,2)
+        return voltage
+        
+    def recvADC(self,channel):
+        if self.Index=="PCF8591":
+            data=self.recvPCF8591(channel)
+        elif self.Index=="ADS7830":
+            data=self.recvADS7830(channel)
+        return data
+    def i2cClose(self):
+        self.bus.close()
+
+# Class for buzzer
+# execute module
+GPIO.setwarnings(False)
+Buzzer_Pin = 17
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(Buzzer_Pin,GPIO.OUT)
+class Buzzer:
+    def run(self,command):
+        if command!=0:
+            GPIO.output(Buzzer_Pin,True)
+        else:
+            GPIO.output(Buzzer_Pin,False)
+
+# Class for Line_Tracking module
+# sensor module
+# channel: light, right, power
+class Line_Tracking:
+    def __init__(self):
+        self.IR01 = 14
+        self.IR02 = 15
+        self.IR03 = 23
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(self.IR01,GPIO.IN)
+        GPIO.setup(self.IR02,GPIO.IN)
+        GPIO.setup(self.IR03,GPIO.IN)
+    def signal(self):
+        return [(bool)(GPIO.input(self.IR01)),
+                    (bool)(GPIO.input(self.IR02)),
+                    (bool)(GPIO.input(self.IR03))]
+
+# Class for servo (need adding offset )
+# executer module
+#   channel: only 0 and 1 is used
+class Servo:
+    def __init__(self):
+        self.PwmServo = PCA9685(0x40, debug=True)
+        self.PwmServo.setPWMFreq(50)
+        self.PwmServo.setServoPulse(8,1500)
+        self.PwmServo.setServoPulse(9,1500)
+    def setServoPwm(self,channel,angle,error=10):
+        angle=int(angle)
+        if channel=='0':
+            self.PwmServo.setServoPulse(8,2500-int((angle+error)/0.09))
+        elif channel=='1':
+            self.PwmServo.setServoPulse(9,2500-int((angle+error)/0.09))
+        elif channel=='2':
+            self.PwmServo.setServoPulse(10,500+int((angle+error)/0.09))
+        elif channel=='3':
+            self.PwmServo.setServoPulse(11,500+int((angle+error)/0.09))
+        elif channel=='4':
+            self.PwmServo.setServoPulse(12,500+int((angle+error)/0.09))
+        elif channel=='5':
+            self.PwmServo.setServoPulse(13,500+int((angle+error)/0.09))
+        elif channel=='6':
+            self.PwmServo.setServoPulse(14,500+int((angle+error)/0.09))
+        elif channel=='7':
+            self.PwmServo.setServoPulse(15,500+int((angle+error)/0.09))                    
+
+# Class for Ultrasonic (need modify output to distants)
+# sensor module
+#   channel: distants result
+class Ultrasonic:
+    def __init__(self):
+        GPIO.setwarnings(False)
+        self.trigger_pin = 27
+        self.echo_pin = 22
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(self.trigger_pin,GPIO.OUT)
+        GPIO.setup(self.echo_pin,GPIO.IN)
+    def send_trigger_pulse(self):
+        GPIO.output(self.trigger_pin,True)
+        time.sleep(0.00015)
+        GPIO.output(self.trigger_pin,False)
+
+    def wait_for_echo(self,value,timeout):
+        count = timeout
+        while GPIO.input(self.echo_pin) != value and count>0:
+            count = count-1
+     
+    def get_distance(self):
+        distance_cm=[0,0,0,0,0]
+        for i in range(3):
+            self.send_trigger_pulse()
+            self.wait_for_echo(True,10000)
+            start = time.time()
+            self.wait_for_echo(False,10000)
+            finish = time.time()
+            pulse_len = finish-start
+            distance_cm[i] = pulse_len/0.000058
+        distance_cm=sorted(distance_cm)
+        return int(distance_cm[2])
+                
+# Class for LED (assignment)
+#   channel: total 8 LED bulds
+# LED strip configuration:
+LED_COUNT      = 8      # Number of LED pixels.
+LED_PIN        = 18      # GPIO pin connected to the pixels (18 uses PWM!).
+LED_FREQ_HZ    = 800000  # LED signal frequency in hertz (usually 800khz)
+LED_DMA        = 10      # DMA channel to use for generating signal (try 10)
+LED_BRIGHTNESS = 255     # Set to 0 for darkest and 255 for brightest
+LED_INVERT     = False   # True to invert the signal (when using NPN transistor level shift)
+LED_CHANNEL    = 0       # set to '1' for GPIOs 13, 19, 41, 45 or 53
+class Led:    
+    def __init__(self):
+        #Control the sending order of color data
+        self.ORDER = "RGB"  
+        # Create NeoPixel object with appropriate configuration.
+        self.strip = Adafruit_NeoPixel(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS, LED_CHANNEL)
+        # Intialize the library (must be called once before other functions).
+        self.strip.begin()
+        
+    def changeLED(self, led, r, g, b):
+            self.strip.setPixelColorRGB(led, r, g, b)
+            self.strip.show()
+
+        
---
+++ freenove_base/interface2driver.py
@@ -0,0 +1,161 @@
+#!/usr/bin/env python
+
+""" interface2driver
+
+This is a port of the interface2driver.py from EE447 Lab 2 into ROS 2. Certain
+interfaces have been disabled or neglected as they were removed from our
+version of our robot.
+
+"""
+import queue
+import rclpy
+from rclpy.node import Node
+from freenove_interfaces.msg import ADC as ADCMsg, Buzzer as BuzzerMsg, LED as LEDMsg, LineTracker as LineTrackerMsg, Motor as MotorMsg, Servo as ServoMsg
+from freenove_interfaces.srv import Ultrasonic
+from .driver4hardware import *
+
+__author__ = "Eugene Seubert"
+__email__ = "seuberte@uw.edu"
+
+class interface2driver(Node):
+    def __init__(self):
+        super().__init__('interface2driver')
+        #======================
+        # Subscriber 
+        #======================
+        # Motor initialize
+        self.motor = Motor()
+        self.motor.setMotorModel(0,0,0,0)
+        #self.motor_sub = rospy.Subscriber(TOPIC["motor_topic"],motor_msg,
+        #                    self.motor_callback, queue_size=3)        
+        self.motor_sub = self.create_subscription(MotorMsg, 'motor_topic', 
+            self.motor_callback, 10)
+
+        # Servo initialize
+        self.servo = Servo()
+        #self.servo_sub =  rospy.Subscriber(TOPIC["servo_topic"],servo_msg,
+        #                    self.servo_callback, queue_size=3)
+        self.servo_sub = self.create_subscription(ServoMsg, 'servo_topic', 
+            self.servo_callback, 10)
+
+        # Buzzer initialize
+        self.buzzer = Buzzer()
+        #self.buzzer_sub =  rospy.Subscriber(TOPIC["buzzer_topic"],buzzer_msg,
+        #                    self.buzzer_callback, queue_size=3)
+        self.buzzer_sub = self.create_subscription(BuzzerMsg, 'buzzer_topic', 
+            self.buzzer_callback, 10)
+        
+        # LED iniialize
+        self.led = Led()
+        #self.led_sub = rospy.Subscriber(TOPIC["led_topic"], led_msg,
+        #                    self.led_callback, queue_size=3)
+        self.led_sub = self.create_subscription(LEDMsg, 'led_topic', 
+            self.led_callback, 10)
+
+        #======================
+        # Publisher
+        #======================
+        # Line tracking sensor initialize
+        self.line = Line_Tracking()
+        #self.line_tracking_pub = rospy.Publisher(TOPIC["line_tracking_topic"], 
+        #                            line_tracking_msg, queue_size=3)
+        self.line_tracking_pub = self.create_publisher(LineTrackerMsg, 
+            'line_tracking_topic', 10)
+
+        # ADC initialize
+        self.adc = Adc()
+        #self.adc_pub =  rospy.Publisher(TOPIC["adc_topic"],adc_msg, queue_size=3) 
+        self.adc_pub = self.create_publisher(ADCMsg, 'adv_topic', 10)
+
+        #======================
+        # service 
+        #======================
+        # Ultrasonic        
+        #self.ultrasonic = Ultrasonic()
+        #self.ultrasonic_run = rospy.Service(TOPIC["ultrasonic_topic"],ultrasonic_srv,
+        #                        self.ultrasonic_callback)
+        #self.ultrasonic = self.create_service(ultrasonic_topic, 'ultrasonic_srv', self.ultrasonic_callback, 10)
+
+        #Set up rate for update publishers 
+        # Update 30 times per second
+        #self.rate = rospy.Rate(30)
+        timer_freq = 30
+        timer_period = 1/timer_freq
+        self.timer = self.create_timer(timer_period, self.timer_callback)
+
+    # callback motors
+    def motor_callback(self, msg):
+        self.motor.setMotorModel(msg.left_upper_wheel,
+                            msg.left_lower_wheel,
+                            msg.right_upper_wheel,
+                            msg.right_lower_wheel)
+
+    # callback servo
+    def servo_callback(self, msg):   
+        self.servo.setServoPwm('0',msg.horizontal)
+        self.servo.setServoPwm('1',msg.vertical)
+
+    # callback for buzzer
+    def buzzer_callback(self,msg):
+        self.buzzer.run(msg.command)
+
+    # callback for ultrasonic
+    # if request is activate==1, run ultrasonic algorithm
+    #def ultrasonic_callback(self,srv):
+    #    if srv.activate:
+    #        self.ultrasonic.get_distance()
+    #        return self.ultrasonic.get_distance()
+
+    # LED callback
+    def led_callback(self, msg):
+        self.led.changeLED(msg.led, msg.r, msg.g, msg.b)
+    
+    # regularly updating publishers
+    def timer_callback(self):
+        line_msg = LineTrackerMsg()
+        line_msg.left, line_msg.mid, line_msg.right = self.line.signal()
+        self.line_tracking_pub.publish(line_msg)
+
+        volt_msg = ADCMsg()
+        volt_msg.left=self.adc.recvADC(0)
+        volt_msg.right=self.adc.recvADC(1)
+        volt_msg.power=self.adc.recvADC(2)*3
+        self.adc_pub.publish(volt_msg)
+    
+    # This will be invoked before actual shutdown occurs
+    # prevent motor keep running
+    def end(self):
+        self.motor.setMotorModel(0,0,0,0)
+
+def main(args=None):
+    rclpy.init(args=args)
+    print("start interface")
+    
+    interface_for_driver = interface2driver()
+
+    rclpy.spin(interface_for_driver)
+
+    interface2driver.end()
+    interface_for_driver.destroy_node()
+    rclpy.shutdown()
+
+    #rospy.init_node('interface2driver', anonymous=True)
+    #TOPIC ={}
+    #TOPIC["motor_topic"] = rospy.get_param("~motor_topic",'/car/hardware/motor')
+    #TOPIC["servo_topic"] = rospy.get_param("~servo_topic",'/car/hardware/servo')
+    #TOPIC["buzzer_topic"] = rospy.get_param("~buzzer_topic",'/car/hardware/buzzer')
+    #TOPIC["led_topic"] = rospy.get_param("~led_topic",'/car/hardware/led')
+    #TOPIC["line_tracking_topic"] = rospy.get_param("~line_tracking_topic",'/car/hardware/line_tracking')
+    #TOPIC["adc_topic"] = rospy.get_param("~adc_topic",'/car/hardware/adc')
+    #TOPIC["ultrasonic_topic"] = rospy.get_param("~ultrasonic_topic",'/car/hardware/ultrasonic')
+    #run = interface2driver(TOPIC)  
+    # Keeping this node is activated
+    #while not rospy.is_shutdown():        
+    #    run.update()        
+    # prevent motor keep running
+    #run.end()   
+    
+    print("\nexit interface")
+
+if __name__ == '__main__':
+    main()
